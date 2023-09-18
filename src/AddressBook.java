079c7ba58b8d2d1a69e0d4ca10ec1c42e1aee721
imannbrar <imannbrar@cmail.carleton.ca>
2023-09-18 11:46:27 -0400

    git commit

---
+++ src/AddressBook.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+
+public class AddressBook {
+    ArrayList allBuddies = new ArrayList<BuddyInfo>();
+
+    public AddressBook(ArrayList allBuddies) {
+        this.allBuddies = allBuddies;
+    }
+
+    public addBuddy(){
+
+    }
+    public removeBuddy(){
+
+    }
+    public static void main(String[] args){
+        System.out.println("Address Book");
+    }
+}
+
